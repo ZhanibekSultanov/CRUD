dec6fb1a6fb236738eb6723e45ac57286542428d
Zhanibek <janibeksultanov97@gmail.com>
2023-02-18 23:07:33 +0600

    validation for create/update

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,9 +6,9 @@
     <a href="{{ route('users.index') }}" class="btn btn-secondary">Back to users</a>
     <form method="POST"
           @if(isset($user))
-            action="{{ route('users.update', $user) }}"
+          action="{{ route('users.update', $user) }}"
           @else
-            action="{{ route('users.store') }}"
+          action="{{ route('users.store') }}"
           @endif
           class="mt-3">
         @csrf
@@ -18,15 +18,21 @@
         <div class="row">
             <div class="col">
                 <input type="text"
-                       value="{{ isset($user) ? $user->name : null }}"
+                       value="{{ old('name', isset($user) ? $user->name : null) }}"
                        class="form-control" placeholder="Name" aria-label="name" name="name">
+                @error('name')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="row mt-3">
             <div class="col">
                 <input
-                    value="{{ isset($user) ? $user->email : null }}"
+                    value="{{ old('email', isset($user) ? $user->email : null) }}"
                     type="text" class="form-control" placeholder="Email" aria-label="email" name="email">
+                @error('email')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="row mt-4">
